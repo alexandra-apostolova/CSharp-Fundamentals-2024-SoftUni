6bacfcd18579bf1a359cf85ccf0c49b7bbac7c67
alexandra-apostolova <abapostolova@gmail.com>
2024-09-17 19:34:36 +0300

    Update Multiplication Table.cs

--- Intro And Basics Syntax Lab/Multiplication Table.cs
+++ Intro And Basics Syntax Lab/Multiplication Table.cs
@@ -4,5 +4,12 @@ public class Class1
 {
 	public Class1()
 	{
-	}
+        int number = int.Parse(Console.ReadLine());
+
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{number} X {i} = {number * i}");
+        }
+    }
 }
