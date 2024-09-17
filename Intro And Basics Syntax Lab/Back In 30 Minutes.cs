bf59ebb8beca3d5c70a035deb12d676c4a0264af
alexandra-apostolova <abapostolova@gmail.com>
2024-09-17 18:13:27 +0300

    Added first exercises

---
+++ Intro And Basics Syntax Lab/Back In 30 Minutes.cs
@@ -0,0 +1,15 @@
+﻿int hoursInput = int.Parse(Console.ReadLine());
+int minutesInput = int.Parse(Console.ReadLine());
+
+int timeInMinutes = minutesInput + (hoursInput * 60);
+
+int finalTime = timeInMinutes + 30;
+
+int hours = finalTime / 60;
+int minutes = finalTime % 60;
+
+if (hours >= 24)
+    hours = 0;
+
+Console.WriteLine($"{hours}:{minutes:d2}");
+
---
+++ Intro And Basics Syntax Lab/Even Number.cs
@@ -0,0 +1,15 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+while (true)
+{
+    if (number % 2 != 0)
+    {
+        Console.WriteLine("Please write an even number.");
+    }
+    else
+    {
+        Console.WriteLine($"The number is: {Math.Abs(number)}");
+        break;
+    }
+    number = int.Parse(Console.ReadLine());
+}
